2c3f6d86abc6610659ff9c34b4c8052d47443d72
Rudi Noor Evendi <rudynoorevendi@gmail.com>
2018-04-05 22:10:51 +0800

    Membuat Atribut dan Behavior

--- src/praktek/pkg01/Kulkas.java
+++ src/praktek/pkg01/Kulkas.java
@@ -10,5 +10,22 @@ package praktek.pkg01;
  * @author proWindows8.1
  */
 public class Kulkas {
+    String merek;
+    String model;
+    String ukuran;
+    String warna;
+    String harga;
     
+    void cetakInfo(){
+        System.out.println("Merek   :"+merek);
+        System.out.println("Model   :"+model);
+        System.out.println("Ukuran  :"+ukuran);
+        System.out.println("Warna   :"+warna);
+        System.out.println("Harga   :"+harga);
+        
+        System.out.println("");
+        System.out.println("Untuk menyimpan makanan, minuman, dan sayuran");
+        System.out.println("");
+        
+    }
 }
